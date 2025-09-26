b8185159b4f235bd88ed825c4ac78f25ba252a41
Manivannan <yuvanmani007@gmail.com>
2025-09-26 15:37:14 +0530

    email service changed for sendgrid

--- utils/config.js
+++ utils/config.js
@@ -4,10 +4,12 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const JWT_SECRET = process.env.JWT_SECRET;
 const EMAIL_USER = process.env.EMAIL_USER;
 const EMAIL_PASS = process.env.EMAIL_PASS;
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 
 module.exports = {
     MONGODB_URI,
     JWT_SECRET,
     EMAIL_USER,
-    EMAIL_PASS
+    EMAIL_PASS,
+    SENDGRID_API_KEY
 }
