cbf60e656ce4b1ce8bb58e82d90c8b45b56a487c
Manivannan <yuvanmani007@gmail.com>
2025-11-26 13:48:54 +0530

    brevo email provider changed

--- utils/config.js
+++ utils/config.js
@@ -5,11 +5,17 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const EMAIL_USER = process.env.EMAIL_USER;
 const EMAIL_PASS = process.env.EMAIL_PASS;
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+const BREVO_API_KEY = process.env.BREVO_API_KEY;
+const SENDER_NAME = process.env.SENDER_NAME;
+const SENDER_EMAIL = process.env.SENDER_EMAIL;
 
 module.exports = {
     MONGODB_URI,
     JWT_SECRET,
     EMAIL_USER,
     EMAIL_PASS,
-    SENDGRID_API_KEY
+    SENDGRID_API_KEY,
+    BREVO_API_KEY,
+    SENDER_NAME,
+    SENDER_EMAIL
 }
